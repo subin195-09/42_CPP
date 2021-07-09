fbb1d70f0668ed488ec817cb3694ef7f5fe265df
Soobin Kim <skim@c4r2s8.42seoul.kr>
2021-07-09 17:17:56 +0900

    07/09 module01 update

--- module01/ex06/main.cpp
+++ module01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 16:17:31 by skim              #+#    #+#             */
-/*   Updated: 2021/07/09 17:04:02 by skim             ###   ########.fr       */
+/*   Updated: 2021/07/09 17:17:03 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,32 @@ int		main(int ac, char *av[])
 		std::cout << "Wrong Argument" << std::endl;
 		return (1);
 	}
+
 	Karen		karen;
 	std::string levelList[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	int			i = return_index(av[1]);
-
-	if (i < 0)
+	
+	switch (i)
 	{
-		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-		return (0);
-	}
-	for (int j = i; j < 4; j++)
-	{
-		std::cout << "[ " + levelList[j] + " ]" << std::endl;
-		karen.complain(levelList[j]);
-		std::cout << std::endl;
+		case 0:
+			std::cout << "[ " + levelList[0] + " ]" << std::endl;
+			karen.complain(levelList[0]);
+			std::cout << std::endl;
+		case 1:
+			std::cout << "[ " + levelList[1] + " ]" << std::endl;
+			karen.complain(levelList[1]);
+			std::cout << std::endl;
+		case 2:
+			std::cout << "[ " + levelList[2] + " ]" << std::endl;
+			karen.complain(levelList[2]);
+			std::cout << std::endl;
+		case 3:
+			std::cout << "[ " + levelList[3] + " ]" << std::endl;
+			karen.complain(levelList[3]);
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+			break ;
 	}
 	return (0);
 }
