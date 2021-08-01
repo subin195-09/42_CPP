47ed2c6d7feb8a6fde77a53c3f49979ecbd4b050
subin195-09 <k_subin@naver.com>
2021-08-01 18:18:43 +0900

    08/01 module03 ready

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -6,21 +6,25 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 14:10:25 by skim              #+#    #+#             */
-/*   Updated: 2021/07/14 17:18:23 by skim             ###   ########.fr       */
+/*   Updated: 2021/08/01 18:18:11 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int		main(void)
+int main(void)
 {
 	ClapTrap ct1("CT1");
 	ClapTrap ct2("CT2");
 
-	std::cout << ct1;
+	std::cout << ct1 << std::endl;
+	std::cout << ct2 << std::endl;
+
 	ct1.attack(ct2.getName());
 	ct2.takeDamage(ct1.getAttackDamage());
 	ct1.takeDamage(10);
 	ct1.beRepaired(10);
-	std::cout << ct1;
+
+	std::cout << std::endl << ct1;
+	std::cout << std::endl << ct2;
 }
