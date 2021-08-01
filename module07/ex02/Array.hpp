0351d06b3f7a36d9cb5337293bd56b0793eff9ae
subin195-09 <k_subin@naver.com>
2021-08-01 19:26:14 +0900

    08/01 module07 update

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 21:11:16 by skim              #+#    #+#             */
-/*   Updated: 2021/07/31 21:11:58 by skim             ###   ########.fr       */
+/*   Updated: 2021/08/01 19:25:51 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,48 @@
 # define ARRAY_HPP
 
 #include <iostream>
+
+template<typename T>
+class Array{
+	private:
+		T *arr;
+		unsigned int size;
+	public:
+		Array() {};
+		Array(unsigned int n) {
+			arr = new T[n];
+			size = n;
+		}
+		Array(Array<T> const & rhs) { this->operator=(rhs); }
+		~Array() {
+			delete[] arr;
+		};
+
+		Array	&operator=( Array const &rhs ) {
+			if (this->size > 0)
+			{
+				delete[] arr;
+				arr = new T[rhs.size];
+			}
+			size = rhs.size;
+			for (unsigned int i = 0; i < rhs.size; i++)
+				this->arr[i] = rhs.arr[i];
+			return (*this);
+		}
+
+		T	&operator[]( unsigned int index ) {
+			if (index >= size)
+				throw OutIndexException();
+			return arr[index];
+		}
+
+		class		OutIndexException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw() {
+					return ("wrong index");
+				}
+		};
+};
 
 #endif
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 21:11:32 by skim              #+#    #+#             */
-/*   Updated: 2021/07/31 21:11:49 by skim             ###   ########.fr       */
+/*   Updated: 2021/08/01 19:24:51 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,68 @@
 
 int	main(void)
 {
+	Array<int> arrInt(5);
+	Array<int> arrInt2(5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << arrInt[i] << " ";
+		arrInt2[i] = i;
+	}
+	std::cout << std::endl;
+
+	for (int i = 0; i < 5; i++)
+		std::cout << arrInt2[i] << " ";
+	std::cout << std::endl;
+
+	try
+	{
+		arrInt[6];
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	arrInt = arrInt2;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << arrInt[i] << " ";
+	}
+	std::cout << std::endl;
+
+
+	Array<std::string> arrString(5);
+	Array<std::string> arrString2(5);
+
+	std::cout << "string1" << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << arrString[i] << ", ";
+		arrString2[i] = std::to_string(i) + " skim";
+	}
+	std::cout << std::endl;
+	std::cout << "string2" << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << arrString2[i] << ", ";
+	std::cout << std::endl;
+
+	try
+	{
+		arrString[6];
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	arrString = arrString2;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << arrString[i] << " ";
+	}
+	std::cout << std::endl;
+	system("leaks array");
+
 	return (0);
 }
